753bcbc2edd3e54e84dfd76d609b6adf45267134
Shubham-raut <123shubhamraut@gmail.com>
2020-09-20 15:05:24 +0530

    project reformed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home/Home';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faList, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
+import Boards from './../src/pages/Boards/Boards';
+import CreateBoard from './../src/pages/CreateBoard/CreateBoard';
+import Layout from './../src/pages/Layout/Layout';
+import Board from '../src/pages/Board/Board';
+
+library.add(faList, faTrashAlt);
+
 function App() {
+  let routes = (
+    <Switch>
+      <Route path="/createboard" component={CreateBoard}></Route>
+      <Route path="/board/:boardId" component={Board}></Route>
+      <Route path="/" component={Boards}></Route>
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <Layout>
+        {routes}
+      </Layout>
+    </div>
   );
 }
 
-export default App;
+export default withRouter(App);
